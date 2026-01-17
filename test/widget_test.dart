b90687a7188c3b5740d1481d9e74878fcdc9789c
AnasIdrissi2005 <Anasidrissi05@gmail.com>
2026-01-17 12:46:47 +0800

    deploy web app

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:one_minute/main.dart';
-
-void main() {
-  testWidgets('App boots', (WidgetTester tester) async {
-    await tester.pumpWidget(const OneMinuteApp());
-    expect(find.text('One Minute'), findsOneWidget);
-  });
-}
